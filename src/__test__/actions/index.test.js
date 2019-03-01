6d3b3ae1cc8e6bc4ada5c94bf003cf7a97c1c733
박민호 <radlohead@naver.com>
2019-03-01 15:14:06 +0900

    actions 테스트 코드 작성

--- src/__test__/actions/index.test.js
+++ src/__test__/actions/index.test.js
@@ -1,10 +1,49 @@
 import * as Types from '../../actions';
 
-it('changeText test', () => {
+describe('actions test', () => {
     const mockText = 'change text';
-    const mockData = {
-        type: Types.CHANGE_TEXT,
-        changeText: mockText
-    };
-    expect(Types.changeText(mockText)).toEqual(mockData);
+    const mockTodo = {
+        id: '141521515',
+        text: 'mock todo test'
+    }
+
+    it('changeText test', () => {
+        const mockData = {
+            type: Types.CHANGE_TEXT,
+            changeText: mockText
+        };
+        expect(Types.changeText(mockText)).toEqual(mockData);
+    });
+    
+    it('addTodo test', () => {
+        const mockData = {
+            type: Types.ADD_TODO,
+            todo: mockTodo
+        }
+        expect(Types.addTodo(mockTodo)).toEqual(mockData);
+    });
+    
+    it('deleteTodo test', () => {
+        const mockData = {
+            type: Types.DELETE_TODO,
+            todo: mockTodo
+        }
+        expect(Types.deleteTodo(mockTodo)).toEqual(mockData);
+    });
+
+    it('editTodo test', () => {
+        const mockData = {
+            type: Types.EDIT_TODO,
+            editing: mockText
+        }
+        expect(Types.editTodo(mockText)).toEqual(mockData);
+    });
+
+    it('saveTodo test', () => {
+        const mockData = {
+            type: Types.SAVE_TODO,
+            todo: mockTodo
+        }
+        expect(Types.saveTodo(mockTodo)).toEqual(mockData);
+    });
 });
