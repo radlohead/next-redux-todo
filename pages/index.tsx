769785f81cbee97578bdbe785686c7892acfcffd
minho <radlohead@naver.com>
2019-03-21 06:33:35 +0900

    meta tag 적용 seo 100점 달성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
+import Head from 'next/head';
 import App from '../src/components/App';
 
 class Index extends React.Component {
     public render(): JSX.Element {
         return (
             <>
+                <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <meta name="Description" content="Put your description here." />
+                    <title>title test!!!</title>
+                </Head>
                 <App />
             </>
         )
